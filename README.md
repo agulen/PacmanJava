18f2f04a2654126305a831c9de61eda8fe1b1e0e
Altan Gulen <gulenak@gmail.com>
2020-08-19 21:35:48 -0400

    Create README and boiler plate code for part 1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# PacmanJava
-A Java version of Pacman aimed at teaching introductory-level students
+# Pacman Java
+
+Welcome to the Pacman Java lesson plan! I hope you find this repository useful for learning and experimenting with code. Before getting started with coding, you will need to set up a proper Java development environment. To do that, you will need to download and install the latest [Java Development Kit](https://www.oracle.com/java/technologies/javase-downloads.html) and a Java IDE. For the IDE, I would recommend [Eclipse](https://www.eclipse.org/downloads/). 
+
+## Part 1 
+
+### Goal: Render Pacman at the top-left coordinate of the screen
+
+**Steps to accomplish that:**
+- Create a `Pacman` class. It should contain:
+    - Member Variables
+        - An integer for a x-coordinate
+        - An integer for a y-coordinate
+        - An ImageIcon for a Pacman image
+    - Functions
+        - A constructor to initialize the member variables
+        - Getter and setter for each member variable
+        - A `draw` function to draw Pacman to the screen. What paramater will that function need to draw something to the screen?
+
+**You are given:**
+- A `Game` class, which will render a window to the screen for you. Read through the comments to understand how the code works. Try running the program before writing any code to see the window appear.
--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,28 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame; 
 
+// If a class extends JPanel, it is given the ability to render custom graphics to the screen.
+// We will leverage this functionality to create our Pacman game's graphics
 public class Game extends JPanel {
 	private JFrame window = new JFrame("Pacman");
 	
 	public Game() {
-		window.add(this);
-		window.setSize(500, 500); 							   // Sets the size of the window that the Java system creates
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closes the program if user closes the window 
-		window.setVisible(true);							   // Sets the visibility of the window
+		window.add(this);						// Displays our Game class in the window
+		window.setSize(500, 500); 				// Sets the size of the window that the Java system creates
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits the program if user closes the window 
+		window.setVisible(true);				// Sets the visibility of the window
+		window.setBackground(Color.black);		// Sets the background color of the window
 	}
 	
-	
-	public void paint(Graphics g) {
-	    g.drawImage(new ImageIcon("img/pacman.jpg").getImage(), 100, 100, null); 
+	// This method is supplied by the JPanel extension and automatically called
+	// by the Java system. This is where we can put our custom code to render graphics. 
+	// The Graphics parameter is also given to us by the Java system. It supplies us
+	// with many different methods for rendering elements to the screen. 
+	public void paint(Graphics graphics) {
+	    graphics.setColor(Color.green);
+		graphics.drawLine(0, 0, 100, 400);  // Draws a line from (0,0) to (100, 400) 
 	} 
 	
 	public static void main(String[] args) {
