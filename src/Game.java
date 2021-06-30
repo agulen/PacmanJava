3ead97953e3030dffadaea0ef0940c102985f79a
Altan Gulen <algulen@microsoft.com>
2021-06-30 16:12:04 -0400

    add board width/height const

--- src/Game.java
+++ src/Game.java
@@ -12,16 +12,19 @@ import javax.swing.Timer;
 // If a class extends JPanel, it is given the ability to render custom graphics to the screen.
 // We will leverage this functionality to create our Pacman game's graphics
 public class Game extends JPanel implements KeyListener {
+	public static final int BOARD_HEIGHT = 500;
+	public static final int BOARD_WIDTH = 500;
+
 	private JFrame window = new JFrame("Pacman");
 	private Pacman pacman = new Pacman(0, 0, "img/pacman.jpg");
 	private Timer timer; 
 	
 	public Game() {
-		window.add(this);						// Displays our Game class in the window
-		window.setSize(500, 500); 				// Sets the size of the window that the Java system creates
+		window.add(this);									   // Displays our Game class in the window
+		window.setSize(BOARD_WIDTH, BOARD_HEIGHT); 			   // Sets the size of the window that the Java system creates
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits the program if user closes the window 
-		window.setVisible(true);				// Sets the visibility of the window
-		window.setBackground(Color.black);		// Sets the background color of the window
+		window.setVisible(true);							   // Sets the visibility of the window
+		window.setBackground(Color.black);					   // Sets the background color of the window
 		window.addKeyListener(this);
 		initializeAutoPaint(); 
 	}
@@ -59,7 +62,7 @@ public class Game extends JPanel implements KeyListener {
 	// this class as a listener (line 25), this function will be automatically
 	// called whenever a key is pressed. 
 	public void keyPressed(KeyEvent e) {		
-		// TODO. One case is given. Add the other 3. 		
+		// TODO. One case is given. Complete it and add the other 3. 		
 		switch(e.getKeyCode()) {
 			case KeyEvent.VK_LEFT:
 				break;
