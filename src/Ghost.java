77550ff6e1e0acf9cca3cfec93033e39a54e32f6
Altan Gulen <algulen@microsoft.com>
2021-07-03 18:00:38 -0400

    make image member variables final

--- src/Ghost.java
+++ src/Ghost.java
@@ -3,8 +3,8 @@ import javax.swing.ImageIcon;
 
 // The class represents a Ghost character that moves around the game board
 public class Ghost extends Mover {
-	private static Image redLeft  	  = new ImageIcon("img/ghost1red/left.jpg").getImage();
-	private static Image redRight 	  = new ImageIcon("img/ghost1red/right.jpg").getImage();
+	private final static Image redLeft  = new ImageIcon("img/ghost1red/left.jpg").getImage();
+	private final static Image redRight = new ImageIcon("img/ghost1red/right.jpg").getImage();
 	
 	private GhostType ghostType; 
 	
--- src/Pacman.java
+++ src/Pacman.java
@@ -2,10 +2,10 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Pacman extends Mover {
-	private static Image pacmanLeft  = new ImageIcon("img/pacmanleft.jpg").getImage();
-	private static Image pacmanRight = new ImageIcon("img/pacmanright.jpg").getImage();
-	private static Image pacmanUp    = new ImageIcon("img/pacmanup.jpg").getImage();
-	private static Image pacmanDown  = new ImageIcon("img/pacmandown.jpg").getImage();
+	private final static Image pacmanLeft  = new ImageIcon("img/pacmanleft.jpg").getImage();
+	private final static Image pacmanRight = new ImageIcon("img/pacmanright.jpg").getImage();
+	private final static Image pacmanUp    = new ImageIcon("img/pacmanup.jpg").getImage();
+	private final static Image pacmanDown  = new ImageIcon("img/pacmandown.jpg").getImage();
 	
 	public Pacman(int x, int y, Direction direction) {
 		super(x, y, direction);	
